f6fcf6fb67afce032f75fdbb60b3f0e4970e6160
WilliamsOz <williams.ozserttas05@gmail.com>
2022-01-07 22:28:57 +0100

    Save

---
+++ philo.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/07 22:03:15 by user42            #+#    #+#             */
+/*   Updated: 2022/01/07 22:27:56 by wiozsert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO.H
+# define PHILO.H
+
+# include <unistd.h>
+# include <string.h>
+# include <sys/time.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+typedef struct s_philo
+{
+	
+}				t_philo;
+
+int		ft_strlen(char *str);
+void	print_fd(int fd, char *str);
+
+#endif
