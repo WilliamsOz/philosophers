e05bb6d7570fa67d6c362b4bebdfe6cc7ad53152
WilliamsOz <williams.ozserttas05@gmail.com>
2022-04-04 15:17:26 +0200

    End

--- philo/srcs/thread/routine.c
+++ philo/srcs/thread/routine.c
@@ -6,7 +6,7 @@
 /*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 17:11:27 by wiozsert          #+#    #+#             */
-/*   Updated: 2022/04/04 13:43:13 by wiozsert         ###   ########.fr       */
+/*   Updated: 2022/04/04 15:13:13 by wiozsert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,9 +45,9 @@ t_dlk	*do_routine(t_dlk *dlk)
 	pthread_mutex_unlock(&dlk->eat_mutex);
 	print_status(dlk->data, dlk, EAT);
 	usleep(dlk->data->eat);
-	dlk->number_of_meal += 1;
 	pthread_mutex_unlock(&dlk->fork_mutex);
 	pthread_mutex_unlock(&dlk->previous->fork_mutex);
+	dlk->number_of_meal += 1;
 	print_status(dlk->data, dlk, SLEEP);
 	usleep(dlk->data->sleep);
 	print_status(dlk->data, dlk, THINK);
